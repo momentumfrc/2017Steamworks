dda3a9e48729b36d125f88054b024311b41b629c
Jordan Powers <jordanpowers1227@gmail.com>
2017-02-11 14:34:36 -0800

    Progress on distance class

--- 2017 Robot/src/org/usfirst/frc/team4999/robot/Distance.java
+++ 2017 Robot/src/org/usfirst/frc/team4999/robot/Distance.java
@@ -5,7 +5,19 @@ import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import org.usfirst.frc.analog.adis16448.*;
 
 public class Distance {
+	float[] accel;
+	float[] vel;
+	float[] dist;
 	Accelerometer builtIn = new BuiltInAccelerometer();
 	Accelerometer ADXL362 = new ADXL362(Accelerometer.Range.k8G);
 	ADIS16448_IMU adis = new ADIS16448_IMU();
+	int count = 0;
+	
+	public void Distance() {
+		
+	}
+	
+	void updateDistance() {
+		
+	}
 }
