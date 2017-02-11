a62f394322c01419be4a55e2ab328df3258d062a
Jordan Powers <jordanpowers1227@gmail.com>
2017-02-11 14:51:11 -0800

    Get accel values into array

--- 2017 Robot/src/org/usfirst/frc/team4999/robot/Distance.java
+++ 2017 Robot/src/org/usfirst/frc/team4999/robot/Distance.java
@@ -5,19 +5,32 @@ import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import org.usfirst.frc.analog.adis16448.*;
 
 public class Distance {
-	float[] accel;
-	float[] vel;
-	float[] dist;
+	double[] accelsX;
+	double[] accelsY;
+	double[] accelsZ;
 	Accelerometer builtIn = new BuiltInAccelerometer();
 	Accelerometer ADXL362 = new ADXL362(Accelerometer.Range.k8G);
 	ADIS16448_IMU adis = new ADIS16448_IMU();
 	int count = 0;
 	
 	public void Distance() {
-		
+		accelsX = new double[9];
+		accelsY = new double[9];
+		accelsZ = new double[9];
 	}
 	
 	void updateDistance() {
+		for(int i = 0; i < 9; i += 3) {
+			accelsX[i] = builtIn.getX();
+			accelsX[i+1] = ADXL362.getX();
+			accelsX[i+2] = adis.getAccelX();
+			accelsY[i] = builtIn.getY();
+			accelsY[i+1] = ADXL362.getY();
+			accelsY[i+2] = adis.getAccelY();
+			accelsZ[i] = builtIn.getZ();
+			accelsZ[i+1] = ADXL362.getZ();
+			accelsZ[i+2] = adis.getAccelZ();
+		}
 		
 	}
 }
