ff800ff016e0fbb8e090ca1a0e0997a5d3001e3f
Nathan McConnell <nathanmcconnell7@gmail.com>
2017-02-18 10:01:36 -0800

    ;( fixed it again.............

--- 2017 Robot/src/org/usfirst/frc/team4999/robot/Robot.java
+++ 2017 Robot/src/org/usfirst/frc/team4999/robot/Robot.java
@@ -264,7 +264,7 @@ public class Robot extends IterativeRobot {
 			}
 		}*/
 	}catch (NullPointerException e){
-		System.err.print("Something May Be Disconnected. Please Check Connections And Try Again.");
+		System.err.println("Something May Be Disconnected. Please Check Connections And Try Again.");
 	}
 	}
 	
