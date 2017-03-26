925d64fee10969101a42de4a0e97b41999ff98da
nathanmcconnell42 <nathanmcconnell7@gmail.com>
2017-03-26 07:03:52 -0700

    Fixed xErr and added yErr

--- 2017 Robot/src/org/usfirst/frc/team4999/robot/Robot.java
+++ 2017 Robot/src/org/usfirst/frc/team4999/robot/Robot.java
@@ -361,7 +361,8 @@ public class Robot extends IterativeRobot {
 		double distance = ultrasonic.getRangeInches();
 		arcadeDrive(1, xRotationError, .25);
 		if(distance < 2){
-			final int xErr = 0;
+			final double xErr = 160 - cX;
+			final double yErr = 120 - cY;
 			map(xErr, -80, 80, -1, 1);
 			arcadeDrive(1, xErr, .25);
 			if(xErr == 0){
