fca043c209c718e17557559c0b0710cdbb00b032
nathanmcconnell42 <nathanmcconnell7@gmail.com>
2017-06-29 15:49:41 -0700

    Added the 2 encoders

---
+++ 2017 Robot/src/org/usfirst/frc/team4999/robot/beachBlitzAutoCode.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team4999.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+public class beachBlitzAutoCode {
+	public static void main(String[] args){
+		Encoder left = new Encoder(0,1);
+		Encoder right = new Encoder(2,3);
+	}
+}
