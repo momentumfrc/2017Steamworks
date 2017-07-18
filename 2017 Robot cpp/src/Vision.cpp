d44cef4a416b070894d2eaa66e89dfe6f7d84d2a
Jordan Powers <jordanpowers1227@gmail.com>
2017-07-17 23:16:42 -0700

    I really hate opencv

--- 2017 Robot cpp/src/Vision.cpp
+++ 2017 Robot cpp/src/Vision.cpp
@@ -4,7 +4,21 @@
  *  Created on: Jul 17, 2017
  *      Author: jordan
  */
+#include "Vision.h"
 
+Vision::Vision(std::string name, int dev, int w, int h) {
+	this->cam = new cs::UsbCamera(name, dev);
+	this->cam.SetResolution(w,h);
+	CameraInit();
+	imagesink = new cs::CvSink(name + " Sink");
+	imagesink.SetSource(this->cam);
+	CamServ->PutVideo(name + " Source", w, h);
+}
 
+Vision::Vision(std::string name, int dev) {
+	this->Vision(name, dev, 640, 480);
+}
 
-
+void Vision::DrawText(std::string text, int x, int y, double size, double r, double g, double b, int thickness) {
+	cv::putText(cv::InputOutputArray(image), text, new cv::Point(x,y), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, size, new cv::Scalar(r,g,b), thickness, cv::LineTypes::LINE_8, false);
+}
--- 2017 Robot cpp/src/Vision.h
+++ 2017 Robot cpp/src/Vision.h
@@ -9,19 +9,53 @@
 #define SRC_VISION_H_
 
 #include <cscore.h>
-#include <cscore_c.h>
+#include <opencv2/core/core.hpp>
+#include <CameraServer.h>
+#include <opencv2/imgproc/imgproc.hpp>
+
+enum BlurType {BOX, GAUSSIAN, MEDIAN, BILATERAL};
+typedef struct {
+	double min;
+	double max;
+} Range;
 
 class Vision {
 public:
+	Vision(std::string name, int dev, int w, int h);
+	Vision(std::string name, int dev);
+	virtual ~Vision();
 
 protected:
 	cs::UsbCamera cam;
 	cs::CvSink imagesink;
 	cs::CvSource imagesource;
 	cv::Mat image;
-private:
+	std::vector<std::vector<cv::Point>> contours;
 
-}
+	cv::Mat temp;
+
+	virtual void Pocess();
+	virtual void CameraInit();
+
+	void DrawText(std::string text, int x, int y, double size, double r, double g, double b, int thickness);
+	void DrawCircle(cv::Point center, int radius, double r, double g, double b);
+	void DrawCircle(double x, double y, int radius, double r, double g, double b);
+	void DrawContours(double r, double g, double b);
+	void DrawContour(int index, double r, double g, double b);
+	void blur(BlurType type, double doubleRadius);
+	void HsvThreshold(Range hue, Range sat, Range val);
+	void findContours(bool ExternalOnly);
+	void filterContours(double minArea, double minPerimeter, Range Width, Range Height, Range Solidity, Range VertexCount, Range Ratio);
+
+	std::vector<cv::Point> Centers();
+
+	// The first dimension represents each contour, and the second dimension gives the width and height.
+	std::vector<std::vector<double>> wH();
+
+	std::thread start();
+private:
+	frc::CameraServer *CamServ = frc::CameraServer::GetInstance();
+};
 
 
 
